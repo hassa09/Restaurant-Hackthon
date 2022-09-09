4249a0bede902933fb2ce05ce2addf562b2adc0b
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-09 15:15:30 +0100

    items 2 added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import { useEffect } from 'react';
-import Footer from './Component/Footer/Footer';
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { useEffect } from "react";
+import Footer from "./Component/Footer/Footer";
+import Items2 from "./Component/Items/Items2";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Player from "./Components/MainCarousel/Player";
-import Navbar from './Component/NavBar/Navbar';
-import './App.css';
+import Navbar from "./Component/NavBar/Navbar";
+import "./App.css";
 // import Basket from './Component/Basket';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
         <Navbar />
         <Routes>{/* <Route path='/' element={< Navbar />} /> */}</Routes>
       </Router>
       {/* <Basket /> */}
-
+      <Items2 />
       <Footer />
     </div>
   );
--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,41 +1,41 @@
-import React from 'react';
+import React from "react";
 import {
   AiFillFacebook,
   AiFillInstagram,
   AiOutlineTwitter,
-} from 'react-icons/ai';
+} from "react-icons/ai";
 
-import logo from '../../Assets/Images/ciao.png';
+import logo from "../../assets/Images/ciao.png";
 
 const Footer = () => {
   return (
-    <footer className='flex justify-between items-center w-full h-[6rem] px-1 sm:px-4 bg-slate-700 absolute inset-x-0 bottom-0'>
+    <footer className="flex justify-between items-center w-full h-[6rem] px-1 sm:px-4 bg-slate-700 absolute inset-x-0 bottom-0">
       {/* Logo */}
-      <div className='flex flex-row w-1/4 h-auto mt-8 sm:w-56'>
-        <div className='relative w-12 h-12 sm:h-16 sm:w-16'>
-          <img src={logo} alt='logo' />
+      <div className="flex flex-row w-1/4 h-auto mt-8 sm:w-56">
+        <div className="relative w-12 h-12 sm:h-16 sm:w-16">
+          <img src={logo} alt="logo" />
         </div>
       </div>
       {/* Built by and copyright */}
-      <div className='mt-2 text-center bg-white'>
-        <span className='text-xs bg-white sm:text-sm'>Built by Ciao!</span>
-        <p className='text-[.5rem] sm:text-xs bg-white'>Copyright &copy;2022</p>
+      <div className="mt-2 text-center bg-white">
+        <span className="text-xs bg-white sm:text-sm">Built by Ciao!</span>
+        <p className="text-[.5rem] sm:text-xs bg-white">Copyright &copy;2022</p>
       </div>
       {/* Social Icons*/}
-      <div className='flex flex-row justify-end w-1/4 h-auto mt-2 bg-white sm:w-56'>
-        <div className='w-6 h-6 bg-white item sm:w-10 sm:h-8 sm:m2'>
-          <a href='https://www.facebook.com' target='blank'>
-            <AiFillFacebook className='w-6 h-6 mr-4 bg-white rounded-md sm:h-8 sm:w-8 ' />
+      <div className="flex flex-row justify-end w-1/4 h-auto mt-2 bg-white sm:w-56">
+        <div className="w-6 h-6 bg-white item sm:w-10 sm:h-8 sm:m2">
+          <a href="https://www.facebook.com" target="blank">
+            <AiFillFacebook className="w-6 h-6 mr-4 bg-white rounded-md sm:h-8 sm:w-8 " />
           </a>
         </div>
-        <div className='w-6 h-6 bg-white item sm:w-10 sm:h-8'>
-          <a href='https://www.instagram.com' target='blank'>
-            <AiFillInstagram className='w-6 h-6 mr-4 bg-white rounded-md sm:h-8 sm:w-8' />
+        <div className="w-6 h-6 bg-white item sm:w-10 sm:h-8">
+          <a href="https://www.instagram.com" target="blank">
+            <AiFillInstagram className="w-6 h-6 mr-4 bg-white rounded-md sm:h-8 sm:w-8" />
           </a>
         </div>
-        <div className='w-6 h-6 bg-white item sm:w-10 sm:h-8'>
-          <a href='https://www.twitter.com' target='blank'>
-            <AiOutlineTwitter className='w-6 h-6 mr-2 bg-white rounded-md sm:h-8 sm:w-8 ' />
+        <div className="w-6 h-6 bg-white item sm:w-10 sm:h-8">
+          <a href="https://www.twitter.com" target="blank">
+            <AiOutlineTwitter className="w-6 h-6 mr-2 bg-white rounded-md sm:h-8 sm:w-8 " />
           </a>
         </div>
       </div>
