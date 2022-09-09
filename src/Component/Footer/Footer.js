8b9ab87fb09809d4dd1ad02d430b30f0a13a93dc
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-09 11:59:47 +0100

    chnaged margin

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
   return (
     <footer className="flex justify-between items-center w-full h-[6rem] px-1 sm:px-4 bg-slate-700">
       {/* Logo */}
-      <div className="flex flex-row h-auto w-1/4 sm:w-56">
+      <div className="flex flex-row h-auto w-1/4 sm:w-56 mt-8">
         <div className="h-12 w-12 sm:h-16 sm:w-16 relative">
           <img src={logo} alt="logo" />
         </div>
@@ -21,7 +21,7 @@ const Footer = () => {
         <p className="text-[.5rem] sm:text-xs">Copyright &copy;2022</p>
       </div>
       {/* Social Icons*/}
-      <div className="flex flex-row h-auto w-1/4 sm:w-56 justify-end">
+      <div className="flex flex-row h-auto w-1/4 sm:w-56 justify-end mt-2">
         <div className="item w-6 h-6 sm:w-10 sm:h-8 sm:m2">
           <a href="https://www.facebook.com" target="blank">
             <AiFillFacebook className="h-6 w-6 sm:h-8 sm:w-8 rounded-md mr-4 " />
