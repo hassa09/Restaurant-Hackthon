a4582c71e6435aa51389de6ad0062a4da2ef1fae
AfamOkoh <afamokoh@gmail.com>
2022-09-09 14:28:14 +0100

    style: change background colour to white in footer to show small images added

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -5,7 +5,7 @@ import {
   AiOutlineTwitter,
 } from 'react-icons/ai';
 
-import logo from '../../Assets/ciao.png';
+import logo from '../../Assets/Images/ciao.png';
 
 const Footer = () => {
   return (
@@ -13,29 +13,29 @@ const Footer = () => {
       {/* Logo */}
       <div className='flex flex-row w-1/4 h-auto mt-8 sm:w-56'>
         <div className='relative w-12 h-12 sm:h-16 sm:w-16'>
-          {/*    <img src={logo} alt='logo' /> */}
+          <img src={logo} alt='logo' />
         </div>
       </div>
       {/* Built by and copyright */}
-      <div className='mt-2 text-center'>
-        <span className='text-xs sm:text-sm'>Built by Ciao!</span>
-        <p className='text-[.5rem] sm:text-xs'>Copyright &copy;2022</p>
+      <div className='mt-2 text-center bg-white'>
+        <span className='text-xs bg-white sm:text-sm'>Built by Ciao!</span>
+        <p className='text-[.5rem] sm:text-xs bg-white'>Copyright &copy;2022</p>
       </div>
       {/* Social Icons*/}
-      <div className='flex flex-row justify-end w-1/4 h-auto mt-2 sm:w-56'>
-        <div className='w-6 h-6 item sm:w-10 sm:h-8 sm:m2'>
+      <div className='flex flex-row justify-end w-1/4 h-auto mt-2 bg-white sm:w-56'>
+        <div className='w-6 h-6 bg-white item sm:w-10 sm:h-8 sm:m2'>
           <a href='https://www.facebook.com' target='blank'>
-            <AiFillFacebook className='w-6 h-6 mr-4 rounded-md sm:h-8 sm:w-8 ' />
+            <AiFillFacebook className='w-6 h-6 mr-4 bg-white rounded-md sm:h-8 sm:w-8 ' />
           </a>
         </div>
-        <div className='w-6 h-6 item sm:w-10 sm:h-8'>
+        <div className='w-6 h-6 bg-white item sm:w-10 sm:h-8'>
           <a href='https://www.instagram.com' target='blank'>
-            <AiFillInstagram className='w-6 h-6 mr-4 rounded-md sm:h-8 sm:w-8 ' />
+            <AiFillInstagram className='w-6 h-6 mr-4 bg-white rounded-md sm:h-8 sm:w-8' />
           </a>
         </div>
-        <div className='w-6 h-6 item sm:w-10 sm:h-8'>
+        <div className='w-6 h-6 bg-white item sm:w-10 sm:h-8'>
           <a href='https://www.twitter.com' target='blank'>
-            <AiOutlineTwitter className='w-6 h-6 mr-2 rounded-md sm:h-8 sm:w-8 ' />
+            <AiOutlineTwitter className='w-6 h-6 mr-2 bg-white rounded-md sm:h-8 sm:w-8 ' />
           </a>
         </div>
       </div>
