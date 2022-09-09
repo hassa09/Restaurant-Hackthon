d78beb4b3243217e010c2c127ebda51f5e3e7ed3
AfamOkoh <afamokoh@gmail.com>
2022-09-09 13:35:40 +0100

    docs: delete spare components folder

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,40 +1,40 @@
-import React from "react";
+import React from 'react';
 import {
   AiFillFacebook,
   AiFillInstagram,
   AiOutlineTwitter,
-} from "react-icons/ai";
-import logo from "../../assets/ciao.png";
+} from 'react-icons/ai';
+import logo from '../../assets/ciao.png';
 
 const Footer = () => {
   return (
-    <footer className="flex justify-between items-center w-full h-[6rem] px-1 sm:px-4 bg-slate-700">
+    <footer className='flex justify-between items-center w-full h-[6rem] px-1 sm:px-4 bg-slate-700'>
       {/* Logo */}
-      <div className="flex flex-row h-auto w-1/4 sm:w-56 mt-8">
-        <div className="h-12 w-12 sm:h-16 sm:w-16 relative">
-          <img src={logo} alt="logo" />
+      <div className='flex flex-row w-1/4 h-auto mt-8 sm:w-56'>
+        <div className='relative w-12 h-12 sm:h-16 sm:w-16'>
+          <img src={logo} alt='logo' />
         </div>
       </div>
       {/* Built by and copyright */}
-      <div className="mt-2 text-center">
-        <span className="text-xs sm:text-sm">Built by Ciao!</span>
-        <p className="text-[.5rem] sm:text-xs">Copyright &copy;2022</p>
+      <div className='mt-2 text-center'>
+        <span className='text-xs sm:text-sm'>Built by Ciao!</span>
+        <p className='text-[.5rem] sm:text-xs'>Copyright &copy;2022</p>
       </div>
       {/* Social Icons*/}
-      <div className="flex flex-row h-auto w-1/4 sm:w-56 justify-end mt-2">
-        <div className="item w-6 h-6 sm:w-10 sm:h-8 sm:m2">
-          <a href="https://www.facebook.com" target="blank">
-            <AiFillFacebook className="h-6 w-6 sm:h-8 sm:w-8 rounded-md mr-4 " />
+      <div className='flex flex-row justify-end w-1/4 h-auto mt-2 sm:w-56'>
+        <div className='w-6 h-6 item sm:w-10 sm:h-8 sm:m2'>
+          <a href='https://www.facebook.com' target='blank'>
+            <AiFillFacebook className='w-6 h-6 mr-4 rounded-md sm:h-8 sm:w-8 ' />
           </a>
         </div>
-        <div className="item w-6 h-6 sm:w-10 sm:h-8">
-          <a href="https://www.instagram.com" target="blank">
-            <AiFillInstagram className="h-6 w-6 sm:h-8 sm:w-8 rounded-md mr-4 " />
+        <div className='w-6 h-6 item sm:w-10 sm:h-8'>
+          <a href='https://www.instagram.com' target='blank'>
+            <AiFillInstagram className='w-6 h-6 mr-4 rounded-md sm:h-8 sm:w-8 ' />
           </a>
         </div>
-        <div className="item w-6 h-6 sm:w-10 sm:h-8">
-          <a href="https://www.twitter.com" target="blank">
-            <AiOutlineTwitter className="h-6 w-6 sm:h-8 sm:w-8 mr-2 rounded-md " />
+        <div className='w-6 h-6 item sm:w-10 sm:h-8'>
+          <a href='https://www.twitter.com' target='blank'>
+            <AiOutlineTwitter className='w-6 h-6 mr-2 rounded-md sm:h-8 sm:w-8 ' />
           </a>
         </div>
       </div>
