8413aa4f1ffece33ef3435379695b09475474fba
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-09 11:23:43 +0100

    started the footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import Footer from "./Component/Footer/Footer";
 
 function App() {
   useEffect(() => {
-    const url = '';
+    const url = "";
 
     const fetchData = async () => {
       try {
@@ -12,26 +13,28 @@ function App() {
         const json = await response.json();
         console.log(json);
       } catch (error) {
-        console.log('error', error);
+        console.log("error", error);
       }
     };
 
     fetchData();
   }, []);
   return (
-    <div className='App'>
-      <header className='text-blue-500'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="text-blue-500">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
+        <Footer />
       </header>
     </div>
   );
---
+++ src/Component/Footer/Footer.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer className="flex justify-between items-center w-full h-[6rem] px-1 sm:px-4">
+      {/* Logo */}
+      <div className="flex flex-row h-auto w-1/4 sm:w-56">
+        <div className="h-12 w-12 sm:h-16 sm:w-16 relative">
+          <img src="" alt="logo" />
+        </div>
+      </div>
+      {/* Built by and copyright */}
+      <div className="mt-3 sm:mt-6 text-center">
+        <span className="text-xs sm:text-sm">Built by ?</span>
+        <p className="text-[.5rem] sm:text-xs text-gray-50">
+          Copyright &copy;2022
+        </p>
+      </div>
+      {/* Social Icons*/}
+      <div className="flex flex-row h-auto w-1/4 sm:w-56 justify-end">
+        <div className="item w-6 h-6 sm:w-10 sm:h-8 sm:m2">
+          <a href="https://www.facebook.com" target="blank">
+            <AiFillFacebook className="h-6 w-6 sm:h-8 sm:w-8 rounded-md mr-4 " />
+          </a>
+        </div>
+        <div className="item w-6 h-6 sm:w-10 sm:h-8">
+          <a href="https://www.instagram.com" target="blank">
+            <AiFillInstagram className="h-6 w-6 sm:h-8 sm:w-8 rounded-md mr-4 " />
+          </a>
+        </div>
+        <div className="item w-6 h-6 sm:w-10 sm:h-8">
+          <a href="https://www.twitter.com" target="blank">
+            <AiOutlineTwitter className="h-6 w-6 sm:h-8 sm:w-8 mr-2 rounded-md " />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
