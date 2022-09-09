4ec8e721a21cf9d44533d344267445c7e3590d32
Your Name~ <you@example.com>
2022-09-09 11:59:07 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Player from "./Components/MainCarousel/Player";
-import Navbar from "./Component/NavBar/Navbar";
-=======
+
 import logo from './logo.svg';
 import './App.css';
->>>>>>> ff10bbb72ac2f8329c1fe4112581ddff11fd597c
 
 function App() {
   return (
