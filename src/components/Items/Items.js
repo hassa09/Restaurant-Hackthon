e8b8ff826077846ede7917a33755e3ec516f155e
AfamOkoh <afamokoh@gmail.com>
2022-09-09 13:03:42 +0100

    feat: start on specific fetch request for pasta

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -3,9 +3,13 @@ import { useEffect } from 'react';
 
 const Items = () => {
   useEffect(() => {
-    const url =
-      `https://api.spoonacular.com/recipes/716429/information?apiKey=` +
-      `${process.env.REACT_APP_API_KEY}&includeNutrition=true`;
+    const query1 = `https://api.spoonacular.com/recipes/716429/information?apiKey=`;
+
+    const key = `${process.env.REACT_APP_API_KEY}`;
+
+    const query12 = `&includeNutrition=true`;
+
+    const url = `https://api.spoonacular.com/recipes/complexSearch?query=pasta&`;
     const fetchData = async () => {
       try {
         const response = await fetch(url);
