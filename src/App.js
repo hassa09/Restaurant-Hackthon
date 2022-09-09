90a8a7867ef65dd0ec013b747be72c7ab354f201
Your Name~ <you@example.com>
2022-09-09 11:41:32 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Player from "./Components/MainCarousel/Player";
+import Navbar from "./Component/NavBar/Navbar";
 
 function App() {
-  useEffect(() => {
-    const url = '';
-
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        const json = await response.json();
-        console.log(json);
-      } catch (error) {
-        console.log('error', error);
-      }
-    };
-
-    fetchData();
-  }, []);
   return (
-    <div className='App'>
-      <header className='text-blue-500'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* <Route path='/' element={< Navbar />} /> */}
+          
+
+        </Routes>
+
+
+      </Router>
+     
     </div>
   );
 }
---
+++ src/Component/NavBar/Navbar.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { Link } from "react-scroll";
+import { Icon } from '@iconify/react';
+import ResLogo from "../../Assets/Images/ResLogo.jpg"
+import "../NavBar/Navbar.scss"
+
+
+function Navbar() { 
+     const [active,setActive] = useState(false);
+
+    //Navbar icon function 
+    const DisplayMenu = () => {
+        setActive(!active)
+    }
+    return (
+        <div className='header'>
+            <div className='navlogo'>
+                <Link to="/" className="logo">
+                    <img className="logo-image" src={ResLogo} alt="Logo" />
+                </Link>
+            </div>
+            <nav className={active ? 'navbar' : 'closed'}>
+                <ul className='Nav-links'>
+                    <div className='closed'>
+                        <Icon className='close' icon="bi:x-lg" width="100" height="100" onClick={DisplayMenu} />
+                    
+                    </div>
+                    <li className='Nav-links'>
+                        Dishes
+                    </li>
+                    
+                    <li className='Nav-links'>
+                        Basket
+                    </li>
+                
+                </ul>
+            </nav>
+            <nav className='login-navTag'>
+                <ul className='nav-login' >
+                    <li className='nav-login'>
+                            Login
+                    </li>
+
+                    <li className='nav-login'>
+                            Join
+                    </li>
+                </ul>
+
+            </nav>
+            
+            
+            <div className="menubar">
+                <Icon icon="charm:menu-hamburger" className="menu" onClick={DisplayMenu} width="50" height="50" />
+            </div>
+        </div>
+        
+
+    )
+}
+export default Navbar
